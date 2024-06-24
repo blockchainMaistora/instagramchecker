a2843e156903b0e4033e13832ebbd7f30938b35d
blockchainMaistora <80696361+blockchainMaistora@users.noreply.github.com>
2024-06-24 19:17:57 +0200

    🚀

---
+++ createlogfiles.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set logPath=%~dp0
+
+echo Creating instagram_log.txt...
+echo. > "%logPath%instagram_log.txt"
+echo instagram_log.txt created successfully!
+
+echo Creating profile_info.txt...
+echo. > "%logPath%profile_info.txt"
+echo profile_info.txt created successfully!
+
+
+pause
